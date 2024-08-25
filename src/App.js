7f6322339beccecbc7b393556ea9121f9fc92fdf
Shubham R Patil <89505897+shubhamuni@users.noreply.github.com>
2024-08-25 12:38:45 +0530

    Image card maping

--- src/App.js
+++ src/App.js
@@ -1,31 +1,22 @@
-import ssd from "./samsung-memory-m587InP07os-unsplash.jpg"
+import { useEffect, useState } from "react";
+import ImageCard from "./Components/ImageCard";
 function App() {
+  const [images, setImage] = useState([]);
+  const [isLoading, setIsloading] = useState(true);
+  const [term, setTerm] = useState('');
+  
+  useEffect(()=>{
+    console.log("API Key:", process.env.REACT_APP_PIXABAY_API_KEY);
+    fetch(`https://pixabay.com/api/?key=${process.env.REACT_APP_PIXABAY_API_KEY}&q=${term}&image_type=photo&pretty=true`).then(res => res.json()).then(data => {
+      setImage(data.hits); setIsloading(false);
+    }).catch(error => console.log(error))
+  },[]);
   return (
-    <div className="max-w-sm rounded overflow-hidden shadow-lg">
-      <img src={ssd} alt="ssd" className="w-full" />
-      <div className="px-6 py-4">
-        <div className="font-bold text-purple-500 text-xl">
-          Photo by Dipak patil
-        </div>
-        <ul>
-          <li>
-            <strong>Views:</strong>
-            569
-          </li>
-          <li>
-            <strong>Downloads:</strong>
-            120
-          </li>
-          <li>
-            <strong>Likes:</strong>
-            124
-          </li>
-        </ul>
-      </div>
-      <div className="px-6 py-4">
-        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#tag1</span>
-        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#tag2</span>
-        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#tag3</span>
+    <div className="container auto">
+      <div className="grid grid-cols-3 gap-4">
+        {images.map(image => (
+          <ImageCard key={image.id} image={image}/>
+        ))}
       </div>
     </div>
   );
