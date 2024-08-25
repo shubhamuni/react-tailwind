1a95b37751660f579ce74f2765bdbbde70018527
Shubham R Patil <89505897+shubhamuni@users.noreply.github.com>
2024-08-25 13:02:15 +0530

    Loading component

--- src/App.js
+++ src/App.js
@@ -6,18 +6,23 @@ function App() {
   const [term, setTerm] = useState('');
   
   useEffect(()=>{
-    console.log("API Key:", process.env.REACT_APP_PIXABAY_API_KEY);
     fetch(`https://pixabay.com/api/?key=${process.env.REACT_APP_PIXABAY_API_KEY}&q=${term}&image_type=photo&pretty=true`).then(res => res.json()).then(data => {
-      setImage(data.hits); setIsloading(false);
+      setImage(data.hits); setTimeout(() => {
+        setIsloading(false);
+      }, 1000);
     }).catch(error => console.log(error))
   },[]);
   return (
     <div className="container auto">
-      <div className="grid grid-cols-3 gap-4">
-        {images.map(image => (
-          <ImageCard key={image.id} image={image}/>
-        ))}
-      </div>
+      {isLoading ? (
+        <h1 className="text-6xl text-center mx-auto my-10">Loading...</h1>
+      ) : (
+        <div className="grid grid-cols-3 gap-4">
+          {images.map((image) => (
+            <ImageCard key={image.id} image={image} />
+          ))}
+        </div>
+      )}
     </div>
   );
 }
